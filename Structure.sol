5a93fe8fa53abcb5cba8f56f54daa26bcf7ce230
Wafa Hoor <51891659+wafahoor@users.noreply.github.com>
2023-01-31 01:37:54 +0500

    Add files via upload

---
+++ Structure.sol
@@ -0,0 +1,35 @@
+        // SPDX-License-Identifier: GPL-3.0
+
+        pragma solidity 0.8.0;
+
+       struct Student{
+        uint roll;
+        string name;
+
+       }
+
+        contract local{
+                
+        Student public s1;
+
+        constructor (uint _roll, string memory _name)
+        {
+                s1.roll = _roll;
+                s1.name= _name;
+
+        }
+
+        function change(uint _roll, string memory _name) public
+        {
+                Student memory new_student = Student({
+
+                        roll:_roll,
+                        name:_name
+
+                });
+
+                s1=new_student;
+        }
+
+
+          }
