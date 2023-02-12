8fa8843e1cc7ee2392d808e08f94121968b612cb
Wafa Hoor <51891659+wafahoor@users.noreply.github.com>
2023-02-13 02:37:05 +0500

    Add files via upload

---
+++ Contract.sol
@@ -0,0 +1,23 @@
+         
+          // SPDX-License-Identifier: GPL-3.0
+
+          pragma solidity >=0.5.0 < 0.9.0;
+
+
+          contract pay
+     {
+       
+       function payEther() public payable
+       {
+
+       }
+
+       function getBalance() public view returns(uint){
+         return address(this).balance;
+
+       }
+
+     }
+
+
+      
