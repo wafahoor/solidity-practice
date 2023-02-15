9cef5d96b62594ad1c0ccfb9e7a293948b12ccd4
Wafa Hoor <51891659+wafahoor@users.noreply.github.com>
2023-02-16 03:05:29 +0500

    Add files via upload

---
+++ EvenOdd.sol
@@ -0,0 +1,26 @@
+         
+          // SPDX-License-Identifier: GPL-3.0
+
+          pragma solidity >=0.5.0 < 0.9.0;
+
+          contract LargestValue
+     {
+
+  function evenOdd(uint a) public view returns(string memory){
+
+    if(a%2==0){
+      return "even number";
+    }
+
+  else{
+
+    return "odd";
+  }
+
+
+  }
+
+     }
+
+
+      
