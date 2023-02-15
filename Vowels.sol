fe38d89c71ebc00bdf7a0931a1c22a8e4e8ffbb8
Wafa Hoor <51891659+wafahoor@users.noreply.github.com>
2023-02-16 03:28:10 +0500

    Add files via upload

---
+++ Vowels.sol
@@ -0,0 +1,29 @@
+         
+          // SPDX-License-Identifier: GPL-3.0
+
+          pragma solidity >=0.5.0 < 0.9.0;
+
+          contract LargestValue
+     {
+
+  function vowels(string memory Muzukashii) public view returns(uint){
+
+    bytes memory arr = bytes(Muzukashii);
+    uint count;
+    for(uint i=0; i<arr.length; i++){
+
+      if(arr[i]=='a' || arr[i]=='e' || arr[i]=='i' || arr[i]=='o' || arr[i]=='u'){
+        
+        count++;
+
+      }
+    }
+
+return count;
+
+  }
+
+     }
+
+
+      
